3a16069939c6b58c025b6c507e29736aa008868e
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-08-25 12:52:05 +0200

    debug

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('heading', 'DC Comics')</title>
-
+        {{-- Favicon --}}
+        {{-- <link rel="icon" type="image/png" href="../../../public/img/dc-logo.png" sizes="16x16"> --}}
+        <link rel="icon" 
+      type="image/png" 
+      href="http://example.com/myicon.png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
