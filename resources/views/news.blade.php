ce988b8dd60f7ee5fc3ae0fcf90d0f6d7deb1291
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-08-25 11:51:15 +0200

    typo

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,7 +8,7 @@ News | DC Comics
 <section>
     <h1>DC Universe News</h1>
      <article>
-         <h3>Joker is spreading terror</h3>
+         <h3>Joker is spreading terror!</h3>
          <p>Arkham's scariest nightmare is free, <strong>again!</strong></p>
      </article>
 </section>
